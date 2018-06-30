a1d459378d968adf426fabf46e5cd8dce78822a6
Steve Zaske <github@zaske.com>
2018-06-30 16:53:26 -0700

    Added Apollo-boost client and graphql

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+//1
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+// Creating the httpLink that will connect to our server on port 4000
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+// instantiating an Apollo client, by passing the link
+// and the cache
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  })
+
+ReactDOM.render(   
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider> 
+    , document.getElementById('root')
+);
 registerServiceWorker();
